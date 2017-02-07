4f558d2e1bff866a911557cd4cc2ec850170c87e
Edward Barnard <esbarnard@lbl.gov>
2017-02-07 15:23:04 -0800

    Refactor BaseCartesianScan --> BaseRasterScan

--- mcl_stage_slowscan.py
+++ mcl_stage_slowscan.py
@@ -1,17 +1,17 @@
 from __future__ import division, print_function
 import numpy as np
-from ScopeFoundry.scanning.base_cartesian_scan import BaseCartesian2DSlowScan
+from ScopeFoundry.scanning import BaseRaster2DSlowScan
 from ScopeFoundry import Measurement, LQRange
 import time
 
-class MCLStage2DSlowScan(BaseCartesian2DSlowScan):
+class MCLStage2DSlowScan(BaseRaster2DSlowScan):
     
     name = "MCLStage2DSlowScan"
     def __init__(self, app):
-        BaseCartesian2DSlowScan.__init__(self, app, h_limits=(0,75), v_limits=(0,75), h_unit="um", v_unit="um")        
+        BaseRaster2DSlowScan.__init__(self, app, h_limits=(0,75), v_limits=(0,75), h_unit="um", v_unit="um")        
     
     def setup(self):
-        BaseCartesian2DSlowScan.setup(self)
+        BaseRaster2DSlowScan.setup(self)
         #Hardware
         self.stage = self.app.hardware.mcl_xyz_stage
 
