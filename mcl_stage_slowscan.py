963eef9f284105d014354ba690dfd364e2c9bed6
Edward Barnard <esbarnard@lbl.gov>
2017-03-26 21:54:32 -0700

    MCLStage2DSlowScan to allow axis choice, add MCLStage3DStackSlowScan

--- mcl_stage_slowscan.py
+++ mcl_stage_slowscan.py
@@ -1,8 +1,8 @@
 from __future__ import division, print_function
-import numpy as np
-from ScopeFoundry.scanning import BaseRaster2DSlowScan
-from ScopeFoundry import Measurement, LQRange
-import time
+#import numpy as np
+from ScopeFoundry.scanning import BaseRaster2DSlowScan, BaseRaster2DFrameSlowScan
+#from ScopeFoundry import Measurement, LQRange
+#import time
 
 class MCLStage2DSlowScan(BaseRaster2DSlowScan):
     
@@ -12,27 +12,88 @@ class MCLStage2DSlowScan(BaseRaster2DSlowScan):
     
     def setup(self):
         BaseRaster2DSlowScan.setup(self)
+        
+        self.settings.New("h_axis", initial="X", dtype=str, choices=("X", "Y", "Z"))
+        self.settings.New("v_axis", initial="Y", dtype=str, choices=("X", "Y", "Z"))
+        
+        self.ax_map = dict(X=0, Y=1, Z=2)
         #Hardware
         self.stage = self.app.hardware.mcl_xyz_stage
 
-    def move_position_start(self, x,y):
+    def move_position_start(self, h,v):
         #self.stage.y_position.update_value(x)
         #self.stage.y_position.update_value(y)
-        self.stage.move_pos_slow(x,y,None)
+        
+        S = self.settings
+        
+        coords = [None, None, None]
+        coords[self.ax_map(S['h_axis'])] = h
+        coords[self.ax_map(S['v_axis'])] = v
+        
+        #self.stage.move_pos_slow(x,y,None)
+        self.stage.move_pos_slow(*coords)
         self.stage.settings.x_position.read_from_hardware()
         self.stage.settings.y_position.read_from_hardware()
+        self.stage.settings.z_position.read_from_hardware()
     
-    def move_position_slow(self, x,y, dx,dy):
-        #self.stage.y_position.update_value(y)
-        self.stage.move_pos_slow(x,y,None)
-        self.stage.settings.x_position.read_from_hardware()
-        self.stage.settings.y_position.read_from_hardware()
+    def move_position_slow(self, h,v, dh,dv):
+        self.move_position_start(h, v)
 
-    def move_position_fast(self, x,y, dx,dy):
+    def move_position_fast(self,  h,v, dh,dv):
         #self.stage.x_position.update_value(x)
-        self.stage.move_pos_fast(x, y, None)
+        S = self.settings        
+        coords = [None, None, None]
+        coords[self.ax_map(S['h_axis'])] = h
+        coords[self.ax_map(S['v_axis'])] = v
+        self.stage.move_pos_fast(*coords)
+        #self.stage.move_pos_fast(x, y, None)
         #self.current_stage_pos_arrow.setPos(x, y)
         #self.stage.settings.x_position.read_from_hardware()
         #self.stage.settings.y_position.read_from_hardware()
+        
     
+class MCLStage2DFrameSlowScan(BaseRaster2DFrameSlowScan):
     
+    name = "MCLStage2DFrameSlowScan"
+    
+    def __init__(self, app):
+        BaseRaster2DFrameSlowScan.__init__(self, app, h_limits=(0,75), v_limits=(0,75), h_unit="um", v_unit="um")        
+    
+    def setup(self):
+        MCLStage2DSlowScan.setup(self)
+
+    def move_position_start(self, h,v):
+        MCLStage2DSlowScan.move_position_start(self, h, v)
+    
+    def move_position_slow(self, h,v, dh,dv):
+        MCLStage2DSlowScan.move_position_slow(self, h,v, dh,dv)
+        
+    def move_position_fast(self,  h,v, dh,dv):
+        MCLStage2DSlowScan.move_position_fast(self,  h,v, dh,dv)
+        
+        
+class MCLStage3DStackSlowScan(MCLStage2DFrameSlowScan):
+    
+    def setup(self):
+        MCLStage2DFrameSlowScan.setup(self)
+        
+        self.settings.New("stack_axis", initial="Z", dtype=str, choices=("X", "Y", "Z"))
+        self.settings.New_Range('stack', dtype=float)
+        
+        self.settings.stack_num.add_listener(self.settings.n_frames.update_value, int)
+        
+    def on_new_frame(self, frame_i):
+        S = self.settings
+        stack_range = S.ranges['stack']
+        
+        stack_pos_i = stack_range.array[frame_i]
+        coords = [None, None, None]
+        coords[self.ax_map(S['stack_axis'])] = stack_pos_i
+        
+        self.stage.move_pos_slow(*coords)
+        self.stage.settings.x_position.read_from_hardware()
+        self.stage.settings.y_position.read_from_hardware()
+        self.stage.settings.z_position.read_from_hardware()
+        
+
+
